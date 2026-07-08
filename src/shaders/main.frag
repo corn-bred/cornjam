0ff113ef924a93d419d5924f70eced0560e8e226
corn-bred <fakter6@hotmail.com>
2026-07-07 21:58:35 -0400

    Add Animation class

--- src/shaders/main.frag
+++ src/shaders/main.frag
@@ -2,14 +2,29 @@
 
 in vec2 iTexCoords;
 uniform sampler2D Texture;
-uniform vec3 Colour;
+
 uniform bool isSolidColour;
+uniform vec3 Colour;
+
+uniform bool isAnimation;
+uniform int Columns;
+uniform int Rows;
+uniform int Frame;
 
 out vec4 FragColour;
 
 void main() {
     if (isSolidColour) {
         FragColour = vec4(Colour, 1.0);
+    } else if (isAnimation) {
+        vec2 TexSize = vec2(1.0 / Columns, 1.0 / Rows);
+        vec2 TexLocalCoords = iTexCoords * TexSize;
+
+        int Column = Frame % Columns;
+        int Row = Frame / Columns;
+
+        vec2 TexCoords = vec2(TexLocalCoords.x + (TexSize.x * Column), TexLocalCoords.y + (TexSize.y * Row));
+        FragColour = texture(Texture, TexCoords);
     } else {
         FragColour = texture(Texture, iTexCoords);
     }
--- src/spritesheet.h
+++ src/spritesheet.h
@@ -3,6 +3,9 @@
 #include <cornbreadlib/vertexbuffer.h>
 #include <cornbreadlib/shaders.h>
 #include <cmath>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/exponential.hpp>
 
 class Anim_FrameHandler {
     private:
@@ -10,7 +13,7 @@ class Anim_FrameHandler {
     float FrameTime; //Moduloed time, if greater than FrameDuration, reset
     public:
 
-    Anim_FrameHandler(int startframe, int endframe, float frameduration, bool islooping = false){
+    Anim_FrameHandler(int startframe, int endframe, float frameduration, bool islooping = false) {
         StartFrame = startframe;
         EndFrame = endframe;
         FrameDuration = frameduration;
@@ -53,8 +56,59 @@ class Anim_FrameHandler {
         CurrentFrame = StartFrame;
         FrameTime = 0.0f;
     }
+    inline int GetFrame() {
+        return CurrentFrame;
+    }
 };
 
 class Anim_SpriteRenderer {
+    public:
+    TextureBuffer Sprite;
+    Anim_SpriteRenderer(const char *texturepath, GLenum wrappingS = GL_MIRRORED_REPEAT, GLenum wrappingT = GL_MIRRORED_REPEAT, GLenum min = GL_NEAREST_MIPMAP_LINEAR, GLenum mag = GL_LINEAR) : Sprite(texturepath, wrappingS, wrappingT, min, mag) {}
+    Anim_SpriteRenderer(TextureBuffer &sprite) : Sprite(sprite) {}
+    bool RenderSprite(Shader &shader, VertexBuffer &VBO, glm::mat4 Model, glm::mat4 View, glm::mat4 Projection, int Columns, int Rows, int Frame) {
+        shader.use();
+
+        shader.setMat4("model", Model);
+
+        shader.setMat4("view", View);
+
+        shader.setMat4("projection", Projection);
+
+        shader.setBool("isSolidColour", false);
+
+        shader.setBool("isAnimation", true);
+
+        shader.setInt("Columns", Columns);
+        shader.setInt("Rows", Rows);
+
+        shader.setInt("Frame", Frame);
+
+        VBO.bind();
+
+        Sprite.bindTexture(0);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+};
+
+class Animation {
+    public:
+    Anim_FrameHandler FrameHandler;
+    Anim_SpriteRenderer SpriteRenderer;
+    int Columns, Rows;
     
+    Animation(int startframe, int endframe, float frameduration, bool islooping, const char *texturepath, int Columns, int Rows, GLenum wrappingS = GL_MIRRORED_REPEAT, GLenum wrappingT = GL_MIRRORED_REPEAT, GLenum min = GL_NEAREST_MIPMAP_LINEAR, GLenum mag = GL_LINEAR) : FrameHandler(startframe, endframe, frameduration, islooping), SpriteRenderer(texturepath, wrappingS, wrappingT, min, mag) {}
+
+    bool RenderSprite(Shader &shader, VertexBuffer &VBO, glm::mat4 Model, glm::mat4 View, glm::mat4 Projection) {
+        SpriteRenderer.RenderSprite(shader, VBO, Model, View, Projection, Columns, Rows, FrameHandler.GetFrame());
+    }
+
+    void Update(float currentTime, float deltaTime) {
+        FrameHandler.GetCurrentFrame(currentTime, deltaTime);
+    }
+    
+    void PlayAnimation() {
+        FrameHandler.PlayAnimation();
+    }
 };
